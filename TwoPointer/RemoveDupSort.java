e9c944fc39b804cd12abc9a490af0f3b8c23e971
Manoranjan1k <manasj790@gmail.com>
2025-03-06 17:47:41 +0530

    add implementation to remove duplicates from a sorted array

---
+++ TwoPointer/RemoveDupSort.java
@@ -0,0 +1,19 @@
+package TwoPointer;
+
+public class RemoveDupSort {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3,3,4,4,5,5};
+        int ans = removeDup(arr);
+        System.out.println(" All unique Elements " + ans);
+    }
+    static int removeDup(int [] nums){
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
