6ac1a9b018ae0cae38c0f4e77510853d11a30e65
Manoranjan1k <manasj790@gmail.com>
2025-03-07 17:08:53 +0530

    add implementations for partitioning an array by pivot and rotating an array

---
+++ TwoPointer/PatritionArrPivot.java
@@ -0,0 +1,35 @@
+// 2161. Partition Array According to Given Pivot
+package TwoPointer;
+
+public class PatritionArrPivot {
+    public static void main(String[] args) {
+        int[] arr = {-3,4,3,2};
+        int pivot = 2;
+        int [] ans = partition(arr, pivot);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+    static int[] partition(int [] arr, int pivot){
+        
+        int [] ans = new int[arr.length];
+        int start = 0;
+        int end = arr.length - 1;
+       for (int i = 0, j = arr.length-1; i < ans.length; i++,j--) {
+            if(arr[i] < pivot){
+                ans[start] = arr[i];
+                start++;
+            }
+            if(arr[j] > pivot){
+                ans[end] = arr[j];
+                end--;
+            }
+        }
+        while (start <= end) {
+            ans[start] = pivot;
+            start++;
+            
+        }
+        return ans;
+       }   
+}
