0546dd1bdc49cd109787e686a7eabf4e9937d385
Manoranjan1k <manasj790@gmail.com>
2025-11-05 00:11:09 +0530

    refactor TwoSum to use a HashMap for improved performance

--- TwoPointer/TwoSum.java
+++ TwoPointer/TwoSum.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
@@ -8,16 +11,17 @@ public class TwoSum {
         }
     }
     static int[] twoSum(int[] nums, int target){
-        int[] result = new int[2];
-        for(int i = 0; i<nums.length; i++){
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i]+nums[j]==target){
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && map.get(complement) != i){
+               ans = new int[] {map.get(complement), i};
             }
         }
-        return result;
+        return ans;
     }
 }
